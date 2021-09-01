90b69390323748e2de2ff0e4c9072c430fe1c0c1
Ashley Sands <ashley.sands01@gmail.com>
2021-09-01 00:33:06 -0500

    changed photo to image in postcreate

--- client/src/screens/PostCreate/PostCreate.jsx
+++ client/src/screens/PostCreate/PostCreate.jsx
@@ -10,7 +10,7 @@ export default function PostCreate(props) {
     title: '',
     categories: [],
     message: '',
-    photo: ''
+    image: ''
   });
 
   const { handleCreate, locations, currentUser, categories } = props;
